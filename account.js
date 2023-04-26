1f07c598c364eb043736adeb195c957cb5c1ddc6
Jsweety <jpeter.sweety12@protonmail.com>
2023-04-26 16:51:48 +0100

    changed account page

--- account.js
+++ account.js
@@ -3,4 +3,6 @@ function swapLog(id1,id2) {
 	document.getElementById(id2).style.display = "none";
 	document.getElementById(id1+"-text").style.backgroundColor = "red";
 	document.getElementById(id2+"-text").style.backgroundColor = "black";
+	document.getElementById('login').reset();
+	document.getElementById('register').reset();
 }
